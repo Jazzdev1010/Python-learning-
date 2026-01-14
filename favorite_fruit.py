2fb3f8b22d1cf8681f51dbbbd99eceabab4f32f0
Suchi <suchismitamaharana2000@gmail.com>
2026-01-14 18:57:14 +0900

    added day 5 codes

---
+++ favorite_fruit.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "orange", "grape", "mango"]
+
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+fruits.append("pineapple")
+
+print("Updated fruit list:", fruits)
