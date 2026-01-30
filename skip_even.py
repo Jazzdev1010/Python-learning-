dfdeb7700835b5f6041e275a02c3e588212c1657
Suchi <suchismitamaharana2000@gmail.com>
2026-01-30 13:34:42 +0900

    Day 10 files

---
+++ skip_even.py
@@ -0,0 +1,6 @@
+digit = 0
+while digit < 11:
+  digit += 1
+  if digit % 2 ==0:
+    continue
+  print(digit)
---
+++ star_pattern.py
@@ -0,0 +1,5 @@
+for i in range(1, 9):
+    for j in range(i):
+        print("*", end="")
+    #print without any text prints default ending, a newline.
+    print() 
